e9fbd8f0265f402b09f2ccd3ce7cdf980acd7564
Traja <traja@traja.cz>
2021-04-29 22:11:30 +0200

    quite dynamic solution

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,28 +5,56 @@ import Post from '../components/post'
 
 import styles from './index.module.scss'
 
-export default class Home extends Component {  
+export default class Home extends Component { 
   render() {
+    const posts = [
+      {
+        title: 'My first blog post',
+        key: 'a1',
+        paragraphs: [
+          'Hello there',
+          'This is an example of a componentized blog post'
+        ]
+      }, {
+        title: 'My second blog post',
+        key: 'a2',
+        paragraphs: [
+          'Hello there',
+          'This is another example.'
+        ]
+      }, {
+        title: 'The final blog post',
+        key: 'a3',
+        paragraphs: [
+          'C’est fin'
+        ]
+      }
+    ];
+    
     return (
       <div className={styles.main}>
-        <Post title="My first blog post">
-          <P>Hello there</P>
-          <P>This is an example of a componentized blog post</P>
-        </Post>
-  
-        <hr className={styles.hr} />
-  
-        <Post title="My second blog post">
-          <P>Hello there</P>
-          <P>This is another example.</P>
-          <P>Wa-hoo!</P>
-        </Post>
-  
-        <hr className={styles.hr} />
-  
-        <Post title="The final blog post">
-          <P>C’est fin</P>
-        </Post>
+        {posts.map(post => {
+          return (
+            <Post title={post.title} key={post.key}>
+              {post.paragraphs.map((paragraph, index) => {
+                return (
+                  <P key={index.toString()}>{paragraph}</P>
+                )
+              })}
+            </Post>
+          )
+        })
+        .reduce((acc, curr) => {
+          return (
+            <>
+              {acc}
+              <hr className={styles.hr} />
+              {curr}
+            </>
+          )
+        })
+        }
+        
       </div>
     )
   }
